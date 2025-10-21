cd36ce94de22dcb201f89385e46cea5bae6df147
Nella1414 <marianella.polo-p@mail.escuelaing.edu.co>
2025-10-21 11:29:44 -0500

    feat(proxy): migrate lucas sequence UI and assets to proxy module

--- proxy/src/main/resources/static/js/lucasseq.js
+++ proxy/src/main/resources/static/js/lucasseq.js
@@ -7,12 +7,12 @@ function calcularLucas() {
     let posicion = document.getElementById("numero").value;
 
     const peticionHttp = new XMLHttpRequest();
-    peticionHttp.onload = function() {
+    peticionHttp.onload = function () {
         let respuesta = JSON.parse(this.responseText);
-        let textoMostrar = `Operación: <strong>${respuesta.operation}</strong><br><br>Posición: <strong>${respuesta.position}</strong><br><br>Resultado: <strong>${respuesta.result}</strong>`;
+        let textoMostrar = `Operación: <strong>${respuesta.operation}</strong><br><br>Entrada: <strong>${respuesta.input}</strong><br><br>Resultado: <strong>${respuesta.output}</strong>`;
         document.getElementById("getrespmsg").innerHTML = textoMostrar;
         document.getElementById("getrespmsg").style.display = "block";
     }
-    peticionHttp.open("GET", "/proxy?operation=lucas&position=" + posicion);
+    peticionHttp.open("GET", "/lucasseq?value=" + posicion);
     peticionHttp.send();
 }
